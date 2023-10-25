58dde2e6876fded50cd26670891fc826eacb2bbc
Tommy <tommy.thuy@patroids.com>
2023-10-26 05:19:35 +0700

    fix mongodb

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,38 +1,22 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema(
   {
-    username: {
-      type: String,   
-      required: true
-    },
-    firstName: { 
-      type: String, 
-      required: true 
-    },
-    lastName: { 
-      type: String, 
-      required: true 
-    },   
-    hash: { 
-      type: String, 
-      required: true 
-    },
     fullName: {
       type: String,
-      required: true
+      required: true,
     },
     email: {
       type: String,
       required: true,
-      unique: true
-    },   
+      unique: true,
+    },
     password: {
       type: String,
-      required: true
-    }
+      required: true,
+    },
   },
-  { timestamps: true }
+  { timestamps: true },
 );
 
-export default mongoose.models.User || mongoose.model('User', UserSchema);
+export default mongoose.models.User || mongoose.model("User", UserSchema);
